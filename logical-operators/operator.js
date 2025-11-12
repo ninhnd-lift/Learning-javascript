053b0d753693e3891f7d1e8e7968576f18625b53
NinhND <ninhnd@liftsoft.vn>
2025-11-12 14:12:48 +0700

    feat: Done logical operator in javascript

---
+++ logical-operators/operator.js
@@ -0,0 +1,31 @@
+// COMPARISON OPERATORS - TOÁN TỬ SO SÁNH
+
+// ==  so sánh giá trị; 5=='5' // true
+// === so sánh giá trị và kiểu dữ liệu; 5 =='5' //false
+// != khác(so sánh gíá trị); 5!='5' //false
+// !== khác(so sánh giá trị và kiểu dữ liệu); 5!== '5' //true
+// > lớn hơn; < bé hơn; >= lớn hơn bằng; <= nhỏ hơn hoặc bằng
+
+//LOGICAL OPERATOR - TOÁN TỬ LOGIC
+
+// && và ; (3>2 && 2<3)
+// || hoặc ; (3>2 || 2<3)
+// ! phủ định ; !(5>2) => false
+
+//STRING OPEARATOR - TOÁN TỬ STRING
+
+// + nối chuỗi;
+"Ninh" + "ND"; //NinhND
+
+//TERNARY OPERATOR - TOÁN TỬ 3 NGÔI
+
+condition ? valuetrue : valuefalse;
+
+// OPERATOR(ES6)
+// ?. kiểm tra null trước khi truy cập
+user?.name; // không lỗi nếu name là null
+// ?? nếu null/undefined thì lấy giá trị sau
+name ?? "Guest";
+
+//Arithmetic Operator - Toán tử số học
+//+ - * / % ++ -- **
