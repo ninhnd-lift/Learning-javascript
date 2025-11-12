23274747f2ae59d43226d09e8d6996c04898346b
NinhND <ninhnd@liftsoft.vn>
2025-11-12 11:36:24 +0700

    feat: Add file type.js

---
+++ variables-types/type.js
@@ -0,0 +1,29 @@
+// DATA TYPE
+
+// <---- PRIMITIVE TYPES : THAM TRỊ ----->
+
+// - number: int or float
+// - string: string
+// boolean: true/false
+//null: unknow values
+//undefined: for unassigned values
+//symbol: for unique identifiers
+//bigint: is for interger numbers of arbitrary length
+
+// <------- REFERENCE TYPES: THAM CHIẾU ------->
+
+//object: key -value ------ example: {name:'NinhND',age:22}
+//array: Ordered list ----- example: [1,2,3,4,5]
+//function: A callable block of code ------- function greate(){}
+
+//<------ typeof Operator ------->
+typeof undefined; //undefined
+typeof 0; //number
+typeof true; // bollean
+typeof "NinhND"; //string
+typeof Math; //object
+typeof null; //object
+typeof alert; // function
+typeof Symbol; // symbol
+typeof []; // object
+typeof {}; //object
