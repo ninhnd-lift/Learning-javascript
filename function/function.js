522e847790639c5202a67204c606647710354e86
NinhND <ninhnd@liftsoft.vn>
2025-11-12 15:24:02 +0700

    feat: Add file function.js

---
+++ function/function.js
@@ -0,0 +1,17 @@
+// FUNCTION
+
+// thành phần của 1 function
+// từ khoá function, tham số trong hàm, body xử lý trong hàm, return
+
+// example
+function sumTwo(a, b) {
+  return a + b;
+}
+
+// Function Expression -  Hàm biểu thức
+
+const sayHi = function (name) {
+  console.log("xin chào" + name);
+};
+
+sayHi("Ninh");
